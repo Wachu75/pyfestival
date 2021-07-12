ac6a8e2e4b7be95eaf34ce6f6eaaea7d0d4ce7af
Wachu75 <73335290+Wachu75@users.noreply.github.com>
2021-07-12 11:39:19 +0200

    zadanie3-10

--- tydzien_3/zadanie-domowe-3-10.py
+++ tydzien_3/zadanie-domowe-3-10.py
@@ -7,3 +7,23 @@
 # 80% dobry
 # 90% bardzo dobry
 # 95% celujący
+
+def percentValue(percent):
+    mark = {0: 'niedostateczny',45: 'dopuszczający',55: 'dostateczny',80: 'dobry',90: 'bardzo dobry',95: 'celujący'}
+    keyList = sorted(mark.keys())
+
+    for key, value in enumerate(keyList):
+        print(key)
+        print(value)
+        if value >= percent: print(keyList[key+1])
+        #print(type(percent))
+
+    return None
+
+print(percentValue(45))
+
+# keyList=sorted(d.keys())
+# for i,v in enumerate(keyList):
+#     if v=='eeee':
+#         print d[keyList[i+1]]
+#         print d[keyList[i-1]]
