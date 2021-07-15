6ac5cfa712a215627375326bd06b63f5003e4803
Wachu75 <73335290+Wachu75@users.noreply.github.com>
2021-07-15 06:49:48 +0200

    finished

--- tydzien_3/zadanie-domowe-3-6.py
+++ tydzien_3/zadanie-domowe-3-6.py
@@ -1,2 +1,12 @@
 #Przygotuj funkcję, która za pomocą list comprehension przefiltruje tylko słowa, których długość jest większa od 4
 #i mniejsza od 8.
+
+
+def scopeFromMinToMax(data, startPosition = 4, endPosition = 8):
+
+    a = [data[word] for word in range(len(data)) if len(data[word])>startPosition and len(data[word])<endPosition]# if len(data)>startPosition and len(data)<endPosition]
+
+    return a
+
+print(scopeFromMinToMax(['alk', 'asdwewdfergghy', 'as', 'vfgrty'], 2, 8))
+
