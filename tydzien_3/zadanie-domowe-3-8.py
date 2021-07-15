701eef540c6d7231bcccc60168b33d5f2f7088ad
Wachu75 <73335290+Wachu75@users.noreply.github.com>
2021-07-15 06:21:49 +0200

    finished

--- tydzien_3/zadanie-domowe-3-8.py
+++ tydzien_3/zadanie-domowe-3-8.py
@@ -2,3 +2,15 @@
 #ile kolejnych wyrazów ciągu Fibonacciego ta funkcja zwróci.
 #Ciąg fibonacciego to taki ciąg, gdzie każdy kolejny wyraz
 #jest sumą dwóch poprzednich.
+
+def fibonacci(end):
+    values = [1,1,]
+    index = 1
+    while len(values) <= end-1:
+        temp = values[index] + values[index-1]
+        values.append(temp)
+        index += 1
+    return values
+
+print(fibonacci(10))
+
