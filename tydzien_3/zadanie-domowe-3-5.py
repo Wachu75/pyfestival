9ff663be1bb1ba6304ded6a9e951ab5d55d04a3e
Wachu75 <73335290+Wachu75@users.noreply.github.com>
2021-07-15 06:26:56 +0200

    finished

--- tydzien_3/zadanie-domowe-3-5.py
+++ tydzien_3/zadanie-domowe-3-5.py
@@ -1,3 +1,10 @@
 # Przygotuj funkcje, która za pomocą wyrażenia list comprehension będzie potrafiła przefiltrować liczby parzyste z listy
 # przekazanej w argumencie.
 
+listValues = [1,2,3,4,5,6,7,8,9,10,11,12,13,14]
+
+def filterOfEvenValues(data):
+    return [n for n in data if n % 2 == 0]
+
+print(filterOfEvenValues(listValues))
+
