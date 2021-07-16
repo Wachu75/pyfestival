88707a52672c8d7d34f7cbc8473ba9218b3087f4
Wachu75 <73335290+Wachu75@users.noreply.github.com>
2021-07-16 10:10:47 +0200

    finished

--- tydzien_3/zadanie-domowe-3-7.py
+++ tydzien_3/zadanie-domowe-3-7.py
@@ -1,1 +1,15 @@
 #Przygotuj funkcję która zwróci największy wspólny dzielnik dla dwóch liczb przekazanych w jej argumentach.
+
+def NWD(a, b):
+    while (b != 0):
+        temp = b
+        b = a % b
+        a = temp
+    return a
+
+a = int(input('Podaj pierwszą liczbę: '))
+b = int(input('Podaj drugą liczbę: '))
+
+print(NWD(a,b))
+
+
