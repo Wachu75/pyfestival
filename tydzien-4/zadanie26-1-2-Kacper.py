daedd2acc334172cdeb7e8f8ac12d648f48cd393
Wachu75 <73335290+Wachu75@users.noreply.github.com>
2021-07-17 20:50:15 +0200

    the fie name was changed and a solution to the problem by Kacper was added

--- tydzien-4/zadanie26-1-2-Kacper.py
+++ tydzien-4/zadanie26-1-2-Kacper.py
@@ -1,3 +1,19 @@
+def map_and_filter(numbers: list) -> list:
+    return list(
+        filter(
+            lambda x: x % 2 == 0,
+            map(lambda x: x ** 0.5, numbers)
+        )
+    )
+
+numbers = [1,2,3,4,5,6,7,8,9,10,16,25,36,49]
+
+def test_map_and_filter():
+    a = [16,36,25,49]
+    assert map_and_filter(a) == [4,6]
+
+
+
 def capitalize(word):
     return word[0].upper() + word[1:].lower()
 
