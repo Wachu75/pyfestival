0bf25259326380bacca32a0c3cbca51460a53d6e
Wachu75 <krzysztof-wachowiak@wp.pl>
2021-07-18 19:36:07 +0200

    test

---
+++ enumerate1.py
@@ -0,0 +1,11 @@
+books = 'w pustyni i pusz', 'krzyzacy', 'potop'
+
+for number, book in enumerate(books, start=1):
+    print(book, number)
+    print(number, book)
+
+
+for book in enumerate(books, start=1):
+    print(book)
+
+# cos do gita
