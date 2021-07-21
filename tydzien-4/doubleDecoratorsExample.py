63ded5f6f737f66dc286a207498351e2a7f6a6b5
Wachu75 <krzysztof-wachowiak@wp.pl>
2021-07-21 10:47:25 +0200

    change comments

--- tydzien-4/doubleDecoratorsExample.py
+++ tydzien-4/doubleDecoratorsExample.py
@@ -3,9 +3,11 @@ Taken from our Complete Python Course: https://www.udemy.com/the-complete-python
 
 Purely as an example, a function can have multiple decorators.
 
-Decorators are applied from bottom to top, which means the top decorator is the first one to be evaluated when the function is executed.
+Decorators are applied from bottom to top, which means the top decorator is the first one to be evaluated when
+the function is executed.
 
-In this example, we have two decorators. One checks the user's access_level, the other checks the user's username (must start with the letter 'j').
+In this example, we have two decorators. One checks the user's access_level, the other checks the user's username
+(must start with the letter 'j').
 """
 
 import functools
@@ -55,7 +57,9 @@ When `double_decorator()` runs, this chain of "functions" runs:
 
 user_has_permission -> user_name_starts_with_j -> double_decorator
 
-That is because `user_name_starts_with_j` is the first decorator to be applied. It replaces `double_decorator` by the function it returns.
+That is because `user_name_starts_with_j` is the first decorator to be applied. It replaces `double_decorator` 
+by the function it returns.
 
-Then, `user_has_permission` is applied—and it replaces the function the other decorator returned by the function it returns.
+Then, `user_has_permission` is applied—and it replaces the function the other decorator returned by the function 
+it returns.
 """
