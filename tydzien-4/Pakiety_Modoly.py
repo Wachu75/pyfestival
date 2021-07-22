9a227e21b89af889a8fa01c48f1d6c70b54b3bcb
Wachu75 <73335290+Wachu75@users.noreply.github.com>
2021-07-22 20:59:40 +0200

    lesson ....

---
+++ tydzien-4/Pakiety_Modoly.py
@@ -0,0 +1,9 @@
+# pakiety
+# moduły
+
+# pip freeze - wypisanie wszystkich bibliotek
+# pip freeze > requirements.txt zapisze do pliku wszystkie biblioteki
+
+# pip install -r requirements.txt spowoduje doinstalowanie tych bibliotek których nie ma
+# przydaje się gdy dzielimy się projektem i pobieramy z netu inne projekty nad którymi chcemy pracować
+
