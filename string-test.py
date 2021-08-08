591ca6d19496c31488ff0f922b41d276f666453d
Wachu75 <73335290+Wachu75@users.noreply.github.com>
2021-08-08 09:47:42 +0200

    add next print testing

--- string-test.py
+++ string-test.py
@@ -4,7 +4,7 @@ string jest niemutowaly nie możey go podmieniać możemy przeprowadzić operacj
 
 '''
 
-text = 'world hello'
+text = '  ..  world hello   .'
 text2 = 'cos tam innego'
 test = "asd, gthy, miojeiowe, wwwwxxx"
 text1 = str(text.split(' '))
@@ -22,7 +22,7 @@ print('*' *50)
 print(text)
 print(len(text))
 
-test1 = text.strip()#.split(' ')
+test1 = text.strip(" .") # strip usuwa domyślnie strip() białe znaki przed i za napisem lub znaki zdefiniowane strip(" .") spacje i kropiki
 print('*' *50)
 print(test1)
 print(len(test1))
