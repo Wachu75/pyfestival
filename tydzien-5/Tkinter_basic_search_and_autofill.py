64b79c53fe68305e40ea92057d246f3412a486a0
Wachu75 <73335290+Wachu75@users.noreply.github.com>
2021-08-20 20:07:34 +0200

    basic search and autofill

---
+++ tydzien-5/Tkinter_basic_search_and_autofill.py
@@ -0,0 +1,45 @@
+from tkinter import *
+
+root = Tk()
+root.title('test')
+root.geometry("500x300")
+
+def update(data):
+    my_list.delete(0,END)
+    for item in data:
+        my_list.insert(END, item)
+
+def fillout(e):
+    my_entry.delete(0, END)
+    my_entry.insert(0, my_list.get(ACTIVE))
+
+def check(e):
+    typed = my_entry.get()
+    if typed == '':
+        data = toppings
+    else:
+        data = []
+        for item in toppings:
+            if typed.lower() in item.lower():
+                data.append(item)
+    update(data)
+
+
+my_label = Label(root, text="start",font=("Helvetica", 14), fg="grey")
+my_label.pack(pady=20)
+
+my_entry = Entry(root)
+my_entry.pack()
+
+my_list = Listbox(root, width=50)
+my_list.pack(pady=20)
+
+toppings = ["aaaaaddsa","bvbbbbbbb","cccccdsdcs","jjjjjkkj"]
+
+update(toppings)
+
+my_list.bind("<<ListboxSelect>>", fillout)
+
+my_entry.bind("<KeyRelease>", check)
+
+root.mainloop()
