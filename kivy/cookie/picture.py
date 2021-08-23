041944a9ea031c5491122382ff48d5cbe6f1db76
Wachu75 <73335290+Wachu75@users.noreply.github.com>
2021-08-23 12:15:51 +0200

    next step

--- kivy/cookie/picture.py
+++ kivy/cookie/picture.py
@@ -4,3 +4,10 @@ from kivy.uix.scatter import Scatter
 
 class Picture(Scatter):
     source = StringProperty()
+
+    def on_touch_down(self, touch):
+        if touch.button == "scrolldown":
+            self.scale *= 1.25
+        elif touch.button == "scrollup":
+            self.scale *= 0.8
+        super(Picture, self).on_touch_down(touch)
